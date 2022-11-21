216f25585cd66ca9036e5409b9c3b9a942904757
whonore <wolfhonore@gmail.com>
2022-11-21 11:26:26 -0500

    feat: add initial flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "My flakes";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages = {};
+    });
+}
